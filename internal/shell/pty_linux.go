f552b876c6b84ff69e98962543ce71d9317c64df
Noumena Cambria <noumenacambria@gmail.com>
2026-05-19 18:23:28 -0400

    Fix Linux pty vet failure

--- internal/shell/pty_linux.go
+++ internal/shell/pty_linux.go
@@ -3,7 +3,6 @@
 package shell
 
 /*
-#include <errno.h>
 #include <pty.h>
 */
 import "C"
@@ -18,7 +17,7 @@ func openPty() (masterFile, slaveFile *os.File, err error) {
 	var master C.int
 	var slave C.int
 	if rc := C.openpty(&master, &slave, nil, nil, nil); rc != 0 {
-		return nil, nil, fmt.Errorf("openpty: %w", syscall.Errno(C.errno))
+		return nil, nil, fmt.Errorf("openpty failed with return code %d", int(rc))
 	}
 	return os.NewFile(uintptr(master), "personal-mcp-server-pty-master"), os.NewFile(uintptr(slave), "personal-mcp-server-pty-slave"), nil
 }
